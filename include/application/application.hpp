99cf337969e077ae4f51213ed25eafe51cc41cf8
PerHac13 <abdullah13112004@gmail.com>
2025-01-11 15:16:59 +0530

    chore: dummy structure

--- include/application/application.hpp
+++ include/application/application.hpp
@@ -1,36 +1,41 @@
 #pragma once
-
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-#include <GLFW/glfw3.h>
 #include <string>
+#include <memory>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <CesiumAsync/AsyncSystem.h>
+#include <CesiumIonClient/Connection.h>
+#include <CesiumIonClient/Assets.h>
 
 class Application
 {
+private:
+    GLFWwindow *window;
+    bool shouldClose;
+    int windowWidth;
+    int windowHeight;
+    std::string windowTitle;
+
+    // Cesium components
+    std::shared_ptr<CesiumAsync::AsyncSystem> asyncSystem;
+    std::shared_ptr<CesiumAsync::DefaultAssetAccessor> assetAccessor;
+    std::shared_ptr<CesiumIonClient::Connection> ionConnection;
+    std::vector<CesiumIonClient::Asset> ionAssets;
+    std::string ionAccessToken;
+    bool assetsLoaded;
+
+    static void errorCallback(int error, const char *description);
+    void initWindow();
+    void initImGui();
+    void initCesium();
+    void loadIonAssets();
+    void renderUI();
+
 public:
     Application(int width, int height, const std::string &title);
     ~Application();
 
     void run();
-
-private:
-    void init();
-    void cleanup();
     void update();
     void render();
-    static void glfw_error_callback(int error, const char *description);
-
-    GLFWwindow *window;
-    bool should_close;
-
-    // ImGui state
-    bool show_demo_window;
-    bool show_hello_world;
-    bool checkbox_value;
-    float slider_value;
-
-    const int window_width;
-    const int window_height;
-    const std::string window_title;
 };
--- src/application/application.cpp
+++ src/application/application.cpp
@@ -1,29 +1,35 @@
 #include "application/application.hpp"
 #include <iostream>
-#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 Application::Application(int width, int height, const std::string &title)
-    : window(nullptr), should_close(false), show_demo_window(true), show_hello_world(true),
-      checkbox_value(false), slider_value(0.5f), window_width(width), window_height(height),
-      window_title(title)
+    : windowWidth(width), windowHeight(height), windowTitle(title),
+      shouldClose(false), assetsLoaded(false)
 {
-    init();
+    initWindow();
+    initImGui();
+    initCesium();
 }
 
 Application::~Application()
 {
-    cleanup();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
 
-void Application::glfw_error_callback(int error, const char *description)
+void Application::errorCallback(int error, const char *description)
 {
     std::cerr << "GLFW Error " << error << ": " << description << std::endl;
 }
 
-void Application::init()
+void Application::initWindow()
 {
-    glfwSetErrorCallback(glfw_error_callback);
-
+    glfwSetErrorCallback(errorCallback);
     if (!glfwInit())
     {
         throw std::runtime_error("Failed to initialize GLFW");
@@ -33,39 +39,121 @@ void Application::init()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    window = glfwCreateWindow(window_width, window_height, window_title.c_str(), nullptr, nullptr);
+    window = glfwCreateWindow(windowWidth, windowHeight, windowTitle.c_str(), nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
-        throw std::runtime_error("Failed to create GLFW window");
+        throw std::runtime_error("Failed to create window");
     }
 
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
+}
 
+void Application::initImGui()
+{
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-    io.ConfigWindowsMoveFromTitleBarOnly = true;
 
     ImGui::StyleColorsDark();
-
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330");
 }
-
-void Application::cleanup()
+void Application::initCesium()
 {
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-
-    if (window)
+    try
     {
-        glfwDestroyWindow(window);
+        asyncSystem = std::make_shared<CesiumAsync::AsyncSystem>();
+        assetAccessor = std::make_shared<CesiumAsync::DefaultAssetAccessor>();
+        CesiumIonClient::ApplicationData appData{
+            "Simple Cesium Renderer",
+            "1.0.0"};
+
+        const char *token = std::getenv("CESIUM_ION_TOKEN");
+        if (!token)
+        {
+            throw std::runtime_error("CESIUM_ION_TOKEN environment variable not set");
+        }
+
+        ionAccessToken = token;
+
+        ionConnection = std::make_shared<CesiumIonClient::Connection>(
+            asyncSystem,
+            "Simple Cesium Renderer",
+            "1.0.0",
+            ionAccessToken);
+
+        std::cout << "Cesium Ion connection initialized successfully.\n";
+
+        loadIonAssets();
     }
-    glfwTerminate();
+    catch (const std::exception &ex)
+    {
+        std::cerr << "Error initializing Cesium Ion: " << ex.what() << "\n";
+        throw;
+    }
+}
+
+void Application::loadIonAssets()
+{
+    if (!ionConnection)
+    {
+        std::cerr << "Ion connection is not initialized. Cannot load assets.\n";
+        return;
+    }
+
+    ionConnection->assets().thenInMainThread(
+                               [this](std::vector<CesiumIonClient::Asset> &&assets)
+                               {
+                                   try
+                                   {
+                                       ionAssets = std::move(assets);
+                                       assetsLoaded = true;
+
+                                       std::cout << "Successfully loaded " << ionAssets.size() << " assets from Cesium Ion.\n";
+                                   }
+                                   catch (const std::exception &ex)
+                                   {
+                                       std::cerr << "Error while processing assets: " << ex.what() << "\n";
+                                   }
+                               })
+        .catchInMainThread(
+            [](const std::exception &ex)
+            {
+                std::cerr << "Error fetching assets from Cesium Ion: " << ex.what() << "\n";
+            });
+}
+
+void Application::renderUI()
+{
+    ImGui::Begin("Cesium Ion Assets");
+
+    if (!assetsLoaded)
+    {
+        ImGui::Text("Loading assets...");
+    }
+    else
+    {
+        ImGui::Text("Available Assets:");
+        for (const auto &asset : ionAssets)
+        {
+            if (ImGui::TreeNode(asset.name.c_str()))
+            {
+                ImGui::Text("ID: %ld", asset.id);
+                ImGui::Text("Type: %s", asset.type.c_str());
+                ImGui::Text("Description: %s", asset.description.c_str());
+                ImGui::TreePop();
+            }
+        }
+    }
+
+    ImGui::End();
+
+    // Demo window for reference
+    bool show_demo = true;
+    ImGui::ShowDemoWindow(&show_demo);
 }
 
 void Application::update()
@@ -76,63 +164,27 @@ void Application::update()
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
-    const ImGuiViewport *viewport = ImGui::GetMainViewport();
-    ImGui::SetNextWindowPos(viewport->WorkPos);
-    ImGui::SetNextWindowSize(viewport->WorkSize);
-    ImGui::SetNextWindowViewport(viewport->ID);
-
-    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking |
-                                    ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
-                                    ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
-                                    ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-
-    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-    ImGui::Begin("DockSpace", nullptr, window_flags);
-    ImGui::PopStyleVar();
-
-    ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-    ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_PassthruCentralNode);
-
-    if (ImGui::BeginMenuBar())
-    {
-        if (ImGui::BeginMenu("File"))
-        {
-            if (ImGui::MenuItem("Exit"))
-                should_close = true;
-            ImGui::EndMenu();
-        }
-        ImGui::EndMenuBar();
-    }
-
-    if (show_demo_window)
-        ImGui::ShowDemoWindow(&show_demo_window);
-
-    ImGui::Begin("Hello World Window", &show_hello_world, ImGuiWindowFlags_NoCollapse);
-    ImGui::Text("Hello, World!");
-    if (ImGui::Button("Close"))
-        show_hello_world = false;
-    ImGui::Checkbox("Checkbox", &checkbox_value);
-    ImGui::SliderFloat("Slider", &slider_value, 0.0f, 1.0f);
-    ImGui::End();
-
-    ImGui::End(); // DockSpace
+    renderUI();
 }
 
 void Application::render()
 {
     ImGui::Render();
+
     int display_w, display_h;
     glfwGetFramebufferSize(window, &display_w, &display_h);
     glViewport(0, 0, display_w, display_h);
-    glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
     glfwSwapBuffers(window);
 }
 
 void Application::run()
 {
-    while (!glfwWindowShouldClose(window) && !should_close)
+    while (!glfwWindowShouldClose(window) && !shouldClose)
     {
         update();
         render();
